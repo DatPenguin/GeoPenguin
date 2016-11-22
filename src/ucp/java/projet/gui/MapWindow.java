11303b98c9cb63343b13a3c7ca644e994b61cd0b
DatPenguin <baleditore@gmail.com>
2016-11-22 11:51:16 +0100

    Affichage des cartes des regions et parsing intra-continents

--- src/ucp/java/projet/gui/MapWindow.java
+++ src/ucp/java/projet/gui/MapWindow.java
@@ -5,6 +5,9 @@ import ucp.java.projet.Main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by penguin on 22/11/16.
@@ -13,35 +16,49 @@ public class MapWindow extends JFrame {
 
 
     private JPanel panel = new JPanel();
-    private JLabel background = new JLabel();
+    private JLabel countryMap = new JLabel();
+    private JLabel regionMap = new JLabel();
     private JLabel errorLabel = new JLabel();
+    private String foundMap = new String();
 
     public MapWindow() {
         panel.setLayout(null);
 
         this.setTitle("Carte");
         this.setIconImage(Toolkit.getDefaultToolkit().getImage("resources/penguin.png"));
-        this.setSize(500, 300);
+        this.setSize(1000, 400);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setContentPane(panel);
 
-        background.setBounds(0, -14, 500, 300);
+        countryMap.setBounds(0, -14, 500, 400);
+        regionMap.setBounds(500, -14, 500, 400);
 
         BufferedImage i = null;
+        BufferedImage j = null;
 
         try {
             i = toBufferedImage(new ImageIcon("maps/"
                     + Main.countryList.get(Main.getWindow().getComboBox().getSelectedItem()).getISO2().toLowerCase()
                     + "-map.gif").getImage());
+            mapFinder(new File("locator"), Main.countryList.get(Main.getWindow().getComboBox().getSelectedItem()).getISO2().toLowerCase() + "_large_locator.gif");
+            j = toBufferedImage(new ImageIcon(foundMap).getImage());
+            System.out.println(foundMap);
         } catch (Exception e) {
             System.err.println("Map not found");
         }
 
-        if (i != null)
-            background.setIcon(new ImageIcon(i.getScaledInstance(500, 273, Image.SCALE_SMOOTH)));
-        background.setVisible(true);
-        panel.add(background);
+        if (i != null) {
+            countryMap.setIcon(new ImageIcon(i.getScaledInstance(500, 400, Image.SCALE_SMOOTH)));
+            countryMap.setVisible(true);
+            panel.add(countryMap);
+        }
+        if (j != null) {
+            regionMap.setIcon(new ImageIcon(j.getScaledInstance(500, 400, Image.SCALE_SMOOTH)));
+            regionMap.setVisible(true);
+            panel.add(regionMap);
+        }
+
 
         errorLabel.setBounds(160, 120, 1000, 15);
         errorLabel.setText("Aucune carte trouvable");
@@ -73,4 +90,20 @@ public class MapWindow extends JFrame {
         return bimage;
     }
 
+
+    public void mapFinder(File folder, String mapName) {
+        String[] files = folder.list();
+        if (Arrays.asList(files).contains(mapName)) {
+            try {
+                foundMap = folder.getCanonicalPath() + "/" + mapName;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            for (File f : folder.listFiles())
+                if (f.isDirectory())
+                    mapFinder(f, mapName);
+        }
+    }
 }
